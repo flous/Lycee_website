f11af43bcb57dbe61325e2523f1ea5bf71339a97
flous <tebritebri2@gmail.com>
2020-05-11 20:27:22 +0100

    install font Tajawal  and make page rtl direction

--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -121,8 +121,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# إذا أردت عمل مجلد من أجل كل المشروع 
-# STATICFILES_DIRS =[
-#     os.path.join(BASE_DIR , "static"),
-#     'src\static',
-# ] 
+# # إذا أردت عمل مجلد من أجل كل المشروع 
+# # STATICFILES_DIRS =[
+# #     os.path.join(BASE_DIR , "static"),
+# #     'src\static',
+# # ] 
