650175d79a9fe13d3effdf00916798b326db8699
flous <tebritebri2@gmail.com>
2020-05-11 18:56:42 +0100

    install files of css and js and fonts and images
    استخدام
    bootstrap css
    jquery js
    pooper js
    bootstrap js
    fontawesome js
    tajawal   google font

--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog.apps.BlogConfig',
+    'user.apps.UserConfig',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# إذا أردت عمل مجلد من أجل كل المشروع 
+# STATICFILES_DIRS =[
+#     os.path.join(BASE_DIR , "static"),
+#     'src\static',
+# ] 
