56247ead0ec1467f1ae4427f372458c1ab201eac
flous <tebritebri2@gmail.com>
2020-05-16 15:02:59 +0100

    resize image profile using Pillow ( Python Imaging Library)

--- src/user/models.py
+++ src/user/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from PIL import Image
 # Create your models here.
 class Profile(models.Model):
     image =models.ImageField(default='default.jpg', upload_to='profile_pics')
@@ -8,6 +9,16 @@ class Profile(models.Model):
 
     def __str__(self):
         return '{} profile . '.format(self.user.username)
+
+    def save(self , *args , **kwarg):
+        super().save(*args , **kwarg)
+        img = Image.open(self.image.path)
+        if ((img.width > 300) or (img.height > 300)):
+            output_size = (300 , 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+
+
 def Create_profile(sender ,**kwarg):
     if  kwarg['created']:
         Profile.objects.create(user=kwarg['instance'])
