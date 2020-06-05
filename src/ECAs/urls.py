47d7ca5acf926629b30743d4addef054aef5e784
flous <tebritebri2@gmail.com>
2020-06-05 15:47:45 +0100

    authontication and permission and class base view

--- src/ECAs/urls.py
+++ src/ECAs/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-from .views import  delete_images,Types_Activities , Get_Activities , act_detail , ActCreateView , ActUpdateView , ActDeletView ,uplaod_images
+from .views import  Delete_images,Types_Activities , Get_Activities , act_detail , ActCreateView , ActUpdateView , ActDeletView ,Uplaod_images
 urlpatterns  = [
     path('typesactivities/',Types_Activities,name='typesactivities'),
     path('activities/<int:id_act>', Get_Activities , name='activities'),
@@ -9,6 +9,6 @@ urlpatterns  = [
     path ('add_act/',ActCreateView.as_view(), name='add_act'),
     path ('update_act/<slug:pk>',ActUpdateView.as_view(), name='update_act'),
     path('delete_act/<slug:pk>' ,ActDeletView.as_view() , name='delete_act'),
-    path('upload_images/<int:id_act>' ,uplaod_images, name='upload_images'),
-    path('delete_images/<int:id_act>' ,delete_images, name='delete_images'),
+    path('upload_images/<int:id_act>' ,Uplaod_images.as_view(), name='upload_images'),
+    path('delete_images/<int:id_act>' ,Delete_images.as_view(), name='delete_images'),
 ] + static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
--- src/ECAs/views.py
+++ src/ECAs/views.py
@@ -5,6 +5,8 @@ from .models import TypeActivities , Activities , ImagesActivities
 from .forms import AddActtForm
 from django.contrib.auth.models import User ,Group
 from django.urls import reverse
+from django.core.paginator import Paginator , PageNotAnInteger , EmptyPage
+from django.views import View
 
 # Create your views here.
 # من أجل عرض أنواع النشاطات على الصفحة Types_of_activities.html 
@@ -19,9 +21,18 @@ def Types_Activities(request):
 def Get_Activities(request , id_act):
     tyep_acts=get_object_or_404(TypeActivities , pk=id_act)
     acts = tyep_acts.activities.all()
+    paginator = Paginator(acts , 2)
+    page = request.GET.get('page')
+    try:
+        acts=paginator.page(page)
+    except PageNotAnInteger:
+        acts=paginator.page(1)
+    except EmptyPage:
+        acts=paginator.page(paginator.num_page)
     context={
         'title':tyep_acts.title,
         'acts':acts,
+        'type_act':tyep_acts,
     }
     return render(request , 'ECAs/activities.html',context)
 def act_detail(request , id_act):
@@ -36,7 +47,7 @@ def act_detail(request , id_act):
         'imgs_activiti':imgs,  
     }
     return render(request , 'ECAs/act_detail.html', context)
-class ActCreateView(LoginRequiredMixin ,CreateView ):
+class ActCreateView(LoginRequiredMixin ,UserPassesTestMixin,CreateView ):
     model = Activities
     template_name = 'ECAs/add_act.html'
     form_class =AddActtForm
@@ -44,6 +55,12 @@ class ActCreateView(LoginRequiredMixin ,CreateView ):
     def form_valid(self , form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+    def test_func (self):
+        try:
+            group =  Group.objects.get(name='member')
+        except Group.DoesNotExist:
+            return False
+        return group in self.request.user.groups.all()
  # تعديل نشاط موجود مسبقا 
 class ActUpdateView(LoginRequiredMixin ,UserPassesTestMixin,UpdateView):
     model = Activities
@@ -62,32 +79,42 @@ class ActDeletView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
     model = Activities
     success_url = '/'
     def test_func (self):
-        act =self.get_object()
-        if self.request.user == act.author:
-            return True
-        return False
-def uplaod_images(request , id_act):
-    if request.method=='POST':
+        try:
+            group =  Group.objects.get(name='member')
+        except Group.DoesNotExist:
+            return False
+        return group in self.request.user.groups.all()
+
+class Uplaod_images(LoginRequiredMixin,UserPassesTestMixin,View):
+    def get(self, request,id_act, *args, **kwargs):
+        context={'title':'تحميل صور النشاط ','act':id_act,}
+        return render(request , 'ECAs/Uplaoder_images.html',context)
+    def post(self, request,id_act,*args, **kwargs):
         act = Activities.objects.get(id=id_act)
         files = request.FILES.getlist('file-input')
         for f in files:
             instance = ImagesActivities(image=f , activitie=act)
             instance.save()
-        # return redirect('/act_detail/'+str(id_act))
         return HttpResponseRedirect(reverse('act_detail', args=[id_act]))
-    else:
-        context={'title':'تحميل صور النشاط ','act':id_act,}
-        return render(request , 'ECAs/Uplaoder_images.html',context)
-def delete_images(request , id_act):
-    if request.method=='POST':
+    def test_func (self):
+        try:
+            group =  Group.objects.get(name='member')
+        except Group.DoesNotExist:
+            return False
+        return group in self.request.user.groups.all()
+class Delete_images(LoginRequiredMixin,UserPassesTestMixin,View):
+    def get(self, request,id_act, *args, **kwargs):
+        return HttpResponseRedirect(reverse('act_detail', args=[id_act]))
+    def post(self, request,id_act,*args, **kwargs):
         act=Activities.objects.get(id=id_act)
         images=ImagesActivities.objects.filter(activitie=act)
         for img in images:
             if request.POST.get(str(img.pk)):
                 img.delete()
-
-        # return redirect('/act_detail/'+str(id_act))
         return HttpResponseRedirect(reverse('act_detail', args=[id_act]))
-    else:
-        context={'title':'تحميل صور النشاط ','act':id_act,}
-        return render(request , 'ECAs/Uplaoder_images.html',context)
+    def test_func (self):
+        try:
+            group =  Group.objects.get(name='member')
+        except Group.DoesNotExist:
+            return False
+        return group in self.request.user.groups.all()
